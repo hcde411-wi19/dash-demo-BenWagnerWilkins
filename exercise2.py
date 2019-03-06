b527d4f1d426885d209a75dbda39d80f694dcb1c
Ben Wagner-Wilkins <bpww@uw.edu>
2019-03-06 10:46:29 -0800

    fixed

--- exercise2.py
+++ exercise2.py
@@ -10,9 +10,9 @@ import dash_html_components as html
 
 # static data
 years = ['2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']
-Employers_total = [3.684999943, 3.657999992, 3.71600008, 3.832999945, 3.767999887, 3.767999887, 3.70600009, 3.697999954, 3.703000069, 3.673000097, 3.724999905]
-Employment = [49.83800125, 47.95800018, 43.65800095, 41.62200165, 41.91199875, 42.61399841, 43.18999863, 44.31999969, 45.38499832, 46.36299896, 46.59500122]
-
+total_employment = [61.98799896, 61.22700119, 58.37400055, 57.4620018, 57.37599945, 57.8219986, 57.90299988, 58.36999893, 58.74000168, 59.13299942, 59.19200134]
+female_employment = [55.63999939, 55.29399872, 53.46099854, 52.54999924, 52.11299896, 52.22499847, 52.30799866, 52.70800018, 52.94100189, 53.30799866, 53.33200073]
+male_employment = [68.62200165, 67.42299652, 63.50299835, 62.58499908, 62.85800171, 63.64599991, 63.72000122, 64.25099945, 64.75700378, 65.17199707, 65.26300049]
 # TODO: working on this file to add more codes...
 
 # initialize Dash environment
@@ -35,14 +35,15 @@ app.layout = html.Div(children=[
             # configure the data
             'data': [
                 # set x to be weekday, and y to be the counts. We use bars to represent our data.
-                {'x': years, 'y': Employers_total, 'type': 'line', 'name': 'Years'},
-                {'x': years, 'y': Employment, 'type': 'line', 'name': 'Ped S'}
+                {'x': years, 'y': total_employment, 'type': 'line', 'name': 'Total Employment'},
+                {'x': years, 'y': female_employment, 'type': 'line', 'name': 'Female Employment'}
+                {'x': years, 'y': male_employment, 'type': 'line', 'name': 'Male Employment'}
 
             ],
             # configure the layout of the visualization --
             # set the title to be "Usage of the BGT North of NE 70th per week day"
             'layout': {
-                'title': 'Employers Total (Percentage of Population vs. Employment to Population Ratio'
+                'title': 'Employment Rates in the US'
             }
         }
     )
